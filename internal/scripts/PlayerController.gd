45a59f5df022f61992f27bf8395ef911ce1b6086
Streamfire <Streamfire@users.noreply.github.com>
2020-02-25 18:14:33 +0100

    better movement no jumping

--- internal/scripts/PlayerController.gd
+++ internal/scripts/PlayerController.gd
@@ -1,33 +1,25 @@
 extends KinematicBody2D
 
 #moving
-#export(float) var moveForce = 5.0
-export(float) var maxMoveSpeed = 5.0
+export(float) var MoveSpeed = 50.0
+export(float) var Acceleration = 0.2
+export(float) var Deacceleration = 0.7
 #jumping
-export(float) var jumpForce=2000.0
-export(float) var maxFallSpeed = 5.0
-export(float) var gravity=2000.0
-export(int) var MaxJumpCount=2
-#sliding
-const maxWalkableSlope= 60
+export(float) var jumpForce=250.0
+export(float) var gravity=200.0
 
 
-var jump_count = 0
 var inputMotion = Vector2()
 var velocity = Vector2()
-var motion = Vector2()
 
-# Called when the node enters the scene tree for the first time.
+
+
 func _ready():
 	Global.Playernode = self
-	pass # Replace with function body.
 
 
 func _process(_delta):
-	if jump_count < MaxJumpCount and Input.is_action_just_pressed("Move_Jump"):
-		inputMotion.y = -jumpForce
-		jump_count += 1
-	
+	#walking
 	if Input.is_action_pressed("Move_Left"):
 		inputMotion.x= -1
 	elif Input.is_action_pressed("Move_Right"):
@@ -36,14 +28,11 @@ func _process(_delta):
 		inputMotion.x=0
 
 
-
 func _physics_process(delta):
-
-	# SPEED
-	velocity.x = inputMotion.x * maxMoveSpeed
-	velocity.y += gravity
-	if velocity.y > maxFallSpeed:
-		velocity.y = maxFallSpeed
-	
+	if inputMotion.x != 0:
+		velocity.x = lerp(velocity.x, inputMotion.x * MoveSpeed, Acceleration)
+	else:
+		velocity.x = lerp(velocity.x, 0, Deacceleration)
+		
 	move_and_slide(velocity*delta)
 	pass
